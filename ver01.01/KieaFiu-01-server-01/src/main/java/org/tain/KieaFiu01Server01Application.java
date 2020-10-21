3cc447efda784a87516ad103b145ac18f76986ad
grtlinux <grtlinux@gmail.com>
2020-10-21 20:48:05 +0900

    create blank

---
+++ ver01.01/KieaFiu-01-server-01/src/main/java/org/tain/KieaFiu01Server01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaFiu01Server01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaFiu01Server01Application.class, args);
+	}
+
+}
---
+++ ver02.01/KieaFiu-02-client-01/src/main/java/org/tain/KieaFiu02Client01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaFiu02Client01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaFiu02Client01Application.class, args);
+	}
+
+}
