b62ec0e43429aa1d604a65b6611092d6657bbb0d
grtlinux <grtlinux@gmail.com>
2020-11-28 15:15:57 +0900

    add FiuType

--- ver01.02/KieaFiu-01-server-02/src/main/java/org/tain/socket/InfoObject.java
+++ ver01.02/KieaFiu-01-server-02/src/main/java/org/tain/socket/InfoObject.java
@@ -1,5 +1,23 @@
 package org.tain.socket;
 
+import org.springframework.stereotype.Component;
+import org.tain.utils.CurrentInfo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Setter
+@Getter
+@Slf4j
 public class InfoObject {
 
+	private String name;
+	
+	//////////////////////////////////////////////////////////////////
+	
+	public InfoObject() {
+		log.info("KANG-20201111 >>>>> {} {}", CurrentInfo.get());
+	}
 }
---
+++ ver02.02/KieaFiu-02-client-02/src/main/java/org/tain/utils/enums/FiuType.java
@@ -0,0 +1,21 @@
+package org.tain.utils.enums;
+
+public enum FiuType {
+	BIZ_OPEN("Biz Open"),
+		FILE_START("File Start"),
+			FILE_SEND_DATA("File Send Data"),
+			FILE_CHECK("File Check"),
+		FILE_FINISH("File Finish"),
+	BIZ_CLOSE("Biz Close"),
+	ERROR_EXCEPTION("Error Exception");
+
+	private String value;
+
+	FiuType(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
