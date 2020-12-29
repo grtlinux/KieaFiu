f68ab9a196e6f1a30d5fd81bafb53606de323476
grtlinux <grtlinux@gmail.com>
2020-12-29 09:31:11 +0900

    ...

--- ver06.02/KieaFiu-06-echo-02/src/org/tain/properties/PropertiesMain.java
+++ ver06.02/KieaFiu-06-echo-02/src/org/tain/properties/PropertiesMain.java
@@ -7,18 +7,17 @@ import java.util.Properties;
 public class PropertiesMain {
 
 	public static void main(String[] args) throws Exception {
-		Properties properties = new Properties();
-		properties.load(new FileInputStream("./echo.properties"));
-		
 		Properties props = System.getProperties();
 		for(Enumeration<?> en = props.propertyNames(); en.hasMoreElements();) {
 			String key = (String)en.nextElement();
 			String value = props.getProperty(key);
 			System.out.println("## " + key + "=" + value);
 		}
-		
 		System.out.println(">>>>> user.dir      = " + String.valueOf(System.getProperty("user.dir")));
 		System.out.println(">>>>> user.name     = " + String.valueOf(System.getProperty("user.name")));
+		
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("./echo.properties"));
 		System.out.println(">>>>> name          = " + String.valueOf(properties.getProperty("name")));
 		System.out.println(">>>>> server.prompt = " + String.valueOf(properties.getProperty("server.prompt")));
 		System.out.println(">>>>> client.prompt = " + String.valueOf(properties.getProperty("client.prompt")));
