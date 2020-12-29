4fb0fec3a411423adfb0bdfaab3d32b1c6ef414c
grtlinux <grtlinux@gmail.com>
2020-12-29 09:29:47 +0900

    add properties sample

---
+++ ver06.02/KieaFiu-06-echo-02/src/org/tain/properties/PropertiesMain.java
@@ -0,0 +1,26 @@
+package org.tain.properties;
+
+import java.io.FileInputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PropertiesMain {
+
+	public static void main(String[] args) throws Exception {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("./echo.properties"));
+		
+		Properties props = System.getProperties();
+		for(Enumeration<?> en = props.propertyNames(); en.hasMoreElements();) {
+			String key = (String)en.nextElement();
+			String value = props.getProperty(key);
+			System.out.println("## " + key + "=" + value);
+		}
+		
+		System.out.println(">>>>> user.dir      = " + String.valueOf(System.getProperty("user.dir")));
+		System.out.println(">>>>> user.name     = " + String.valueOf(System.getProperty("user.name")));
+		System.out.println(">>>>> name          = " + String.valueOf(properties.getProperty("name")));
+		System.out.println(">>>>> server.prompt = " + String.valueOf(properties.getProperty("server.prompt")));
+		System.out.println(">>>>> client.prompt = " + String.valueOf(properties.getProperty("client.prompt")));
+	}
+}
